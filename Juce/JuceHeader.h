1af2ef6fa83c848fde3349f53b26cfb6c17c7d85
Vinnie Falco <vinnie.falco@gmail.com>
2011-08-19 21:45:46 +0000

    Added a new Juce amalgamation compatible with XCode 4 and iOS devices, along with a script for re-generating it.

---
+++ Juce/JuceHeader.h
@@ -0,0 +1,9 @@
+// #include this header in your source files to use Juce
+
+#ifndef __JUCEHEADER_AMALGAMATION_HEADER__
+#define __JUCEHEADER_AMALGAMATION_HEADER__
+
+#include "AppConfig.h"
+#include "juce_amalgamated.h"
+
+#endif
---
+++ Juce/JuceLibraryCode2.cpp
@@ -0,0 +1,5 @@
+// Add this to your project to use Juce
+// MacOS or iOS projects should not include this file.
+
+#include "AppConfig.h"
+#include "juce_amalgamated2.cpp"
