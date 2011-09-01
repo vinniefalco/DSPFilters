14340677e260c0910ac7582a3f8eef85e14f5bc8
Vinnie Falco <vinnie.falco@gmail.com>
2011-09-01 04:58:55 +0000

    Moving the entire source tree down one directory level to accommodate user-specific xcconfigs.

--- JuceAmalgam/JuceHeader.h
+++
@@ -1,9 +0,0 @@
-// #include this header in your source files to use Juce
-
-#ifndef __JUCEHEADER_AMALGAMATION_HEADER__
-#define __JUCEHEADER_AMALGAMATION_HEADER__
-
-#include "AppConfig.h"
-#include "juce_amalgamated.h"
-
-#endif
